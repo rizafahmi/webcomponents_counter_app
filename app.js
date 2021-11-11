17184d0dfc2fd15fe376bbf20dfdaa84c68b06be
Riza Fahmi <rizafahmi@gmail.com>
2021-11-12 06:50:06 +0700

    Finish <template> introduction

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('Web Components!');
+const template = document.querySelector('#counter');
+const root = document.querySelector('#root');
+
+root.appendChild(template.content.cloneNode(true));
+root.appendChild(template.content.cloneNode(true));
+
+for (let i = 0; i < 8; i += 1) {
+  root.appendChild(template.content.cloneNode(true));
+}
