e294372d03999214f31e514b4ef17e902d424641
Riza Fahmi <rizafahmi@gmail.com>
2021-11-12 07:25:13 +0700

    Reusable custom element

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const template = document.querySelector('#counter');
 const root = document.querySelector('#root');
 
-root.appendChild(template.content.cloneNode(true));
-root.appendChild(template.content.cloneNode(true));
+const counter = document.createElement('c-ounter');
+root.appendChild(counter);
 
 for (let i = 0; i < 8; i += 1) {
-  root.appendChild(template.content.cloneNode(true));
+  const counter = document.createElement('c-ounter');
+  root.appendChild(counter);
 }
