a2071daf300e76fab06fa3d38af8236ce5356f00
Riza Fahmi <rizafahmi@gmail.com>
2021-11-12 13:59:00 +0700

    Finishing Counter App

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const template = document.querySelector('#counter');
 const root = document.querySelector('#root');
 
 const counter = document.createElement('c-ounter');
+counter.setAttribute('value', 19);
 root.appendChild(counter);
 
 for (let i = 0; i < 8; i += 1) {
   const counter = document.createElement('c-ounter');
+  counter.setAttribute('value', i);
+  console.log(counter);
   root.appendChild(counter);
 }
--- counter.js
+++ counter.js
@@ -1,14 +1,52 @@
 class Counter extends HTMLElement {
   constructor() {
     super();
-    this.state = {
-      countNumber: 0,
-    };
-  }
-  connectedCallback() {
+
     const template = document.querySelector('#counter');
     const shadow = this.attachShadow({ mode: 'open' });
+
     shadow.appendChild(template.content.cloneNode(true));
+
+    this.digit = shadow.querySelector('.digit');
+    this.incrementButton = shadow.querySelector('.increment');
+    this.decrementButton = shadow.querySelector('.decrement');
+
+    this.handleIncrementButton = this.handleIncrementButton.bind(this);
+    this.handleDecrementButton = this.handleDecrementButton.bind(this);
+  }
+  connectedCallback() {
+    this.digit.innerHTML = this.value;
+
+    this.incrementButton.addEventListener('click', this.handleIncrementButton);
+    this.decrementButton.addEventListener('click', this.handleDecrementButton);
+  }
+  disconnectedCallback() {
+    this.incrementButton.removeEventListener(
+      'click',
+      this.handleIncrementButton
+    );
+    this.decrementButton.removeEventListener(
+      'click',
+      this.handleDecrementButton
+    );
+  }
+  attributeChangedCallback(name, oldVal, newVal) {
+    this.digit.innerHTML = this.value;
+  }
+  get value() {
+    return this.getAttribute('value');
+  }
+  set value(val) {
+    this.setAttribute('value', val);
+  }
+  static get observedAttributes() {
+    return ['value'];
+  }
+  handleIncrementButton(e) {
+    this.value = +this.value + 1;
+  }
+  handleDecrementButton(e) {
+    this.value = +this.value - 1;
   }
 }
 
